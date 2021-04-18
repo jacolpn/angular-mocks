a25a72151c139ab1a6b23227911de8a124456ec4
jacolpn <jacolpn@outlook.com>
2021-04-17 21:54:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import * as data from '../assets/db.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-mocks';
+  dbJson = data.jack;
 }
